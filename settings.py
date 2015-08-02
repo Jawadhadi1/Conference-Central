b0e86de2d5f325bb25bfc5e3cae859c896f315ed
Jawad Hadi <jawadhadi123@hotmail.com>
2015-08-02 20:43:41 +0100

    Update settings.py

--- settings.py
+++ settings.py
@@ -13,6 +13,6 @@ created/forked from conference.py by wesc on 2014 may 24
 # Replace the following lines with client IDs obtained from the APIs
 # Console or Cloud Console.
 WEB_CLIENT_ID = '72707483470-2m08ap02296nnj2jdp8etn7u49g5r912.apps.googleusercontent.com'
-ANDROID_CLIENT_ID = 'replace with Android client ID'
-IOS_CLIENT_ID = 'replace with iOS client ID'
+ANDROID_CLIENT_ID = 'android-client-id'
+IOS_CLIENT_ID = 'ios-client-id'
 ANDROID_AUDIENCE = WEB_CLIENT_ID
